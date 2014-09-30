efb5d8b057a13be2116ab9a5c7e6a31d031f2d80
aaronykng <aaronykng@gmail.com>
2014-09-30 00:09:29 -0700

    Update port.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+app.set('port', (process.env.PORT || 3000));
 app.use(express.static('public'));
 app.use(express.static('files'));
 
@@ -28,6 +29,6 @@ io.on('connection', function(socket){
 	});
 });
 
-http.listen(3000, function(){
-	console.log('listening on port 3000');
+http.listen(app.get('port'), function(){
+	console.log('listening on port ' + app.get('port'));
 });
